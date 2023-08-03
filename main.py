fff385cff90d17c3d3e793cb3f71c89b9db95797
fuserojeshpradhananga <rojesh.pradhananga@fusemachines.com>
2023-08-03 08:38:15 +0545

    Commited code using functions

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-num1 = int(input("Enter First Number: "))
-num2 = int(input("Enter Second Number: "))
+def perform_calculation(num1, num2, operator):
 
-print("Enter which operation would you like to perform?")
-ch = input("Enter any of these char for specific operation +,-,*,/: ")
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    else:
+        return None
 
-result = 0
-if ch == '+':
+def main():
+    # Get user input for the two numbers and the arithmetic operator
+    num1 = int(input("Enter First Number: "))
+    num2 = int(input("Enter Second Number: "))
 
-    result = num1 + num2
-elif ch == '-':
-    result = num1 - num2
-elif ch == '*':
-    result = num1 * num2
-elif ch == '/':
-    result = num1 / num2
-else:
-    print("Input character is not recognized!")
+    print("Enter which operation would you like to perform?")
+    operator = input("Enter any of these char for specific operation +, -, *, /: ")
 
-print(num1, ch , num2, ":", result)
+    # Calculate the result using the perform_calculation function
+    result = perform_calculation(num1, num2, operator)
+    
+    # Display the result if the operator is valid; otherwise, show an error message
+    if result is not None:
+        print(f"{num1} {operator} {num2} = {result}")
+    else:
+        print("Input character is not recognized!")
+
+if __name__ == "__main__":
+    main()
+
